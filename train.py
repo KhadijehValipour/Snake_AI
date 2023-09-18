a159b89b10e23113cd5f9f5c8786f3c828b73b4b
KhadijehValipour <khadijehvalipour7777@gamil.com>
2023-09-18 14:08:17 -0800

    update

--- train.py
+++ train.py
@@ -1,22 +1,22 @@
 import tensorflow as tf
 import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
 from sklearn.model_selection import train_test_split
 
 
-
 # load data
-data = pd.read_csv('dataset/dataset.csv' , header=None) 
-data = data.dropna()
-data = data.iloc[1:]
+data = pd.read_csv('dataset/dataset.csv',index_col=False) 
+data.fillna(0,inplace=True)
+data = data.reset_index(drop=True)
 
-X = data.iloc[:,:-1].values  # همه ردیف ها تا ستون اخر
-Y = data.iloc[: , -1].values # همه ردیف ها فقط ستون اخرشون
+X = data.iloc[:,:-1].astype(int).values # همه ردیف ها تا ستون اخر
+Y = data.iloc[: ,-1].astype(int).values # همه ردیف ها فقط ستون اخرشون
 
 
-X_train , X_test ,Y_train ,Y_test = train_test_split(X,Y,test_size=0.2,shuffle=True)
-Y_train = Y_train.reshape(-1,1)
-Y_test  = Y_test.reshape(-1,1)
-print(X_train.shape , X_test.shape , Y_train.shape , Y_test.shape)
+
+X_train , X_test ,Y_train ,Y_test = train_test_split(X,Y, test_size=0.2, shuffle=True)
+
 
 model = tf.keras.models.Sequential([
     tf.keras.layers.Dense(14 , activation='sigmoid') , 
@@ -25,14 +25,16 @@ model = tf.keras.models.Sequential([
     tf.keras.layers.Dense(4 , activation='softmax')
 ])
 
+Y_train= Y_train.reshape(-1,1) 
+
+Y_test= Y_test.reshape(-1,1)
+
 model.compile(optimizer='adam' ,
               loss='sparse_categorical_crossentropy' , 
               metrics=['accuracy']
 )
 
-output = model.fit(X_train,Y_train,epochs=108)
-
-model.evaluate(X_test ,Y_test)
+output = model.fit(X_train,Y_train,epochs=10)
 
 loss , accuracy = model.evaluate(X_test,Y_test)
 
